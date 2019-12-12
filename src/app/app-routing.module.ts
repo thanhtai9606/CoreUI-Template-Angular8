2c80ca5a7cc186d0934d3ebccde9428c41d64c4a
Dung Le <dung.lt.cntt@becamex.com.vn>
2019-12-12 15:51:54 +0700

    Ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BasicComponent } from './components/common/layouts/basic/basic.component';
 import { DashboardComponent } from './components/views/dashboard/dashboard.component';
+import { BlankComponent } from './components/common/layouts/blank/blank.component';
+import { LoginComponent } from './views/login/login.component';
+import { NotFoundComponent } from './components/common/not-found/not-found.component';
 
 
 const routes: Routes = [
@@ -12,6 +15,13 @@ const routes: Routes = [
       { path: 'mainView', component: DashboardComponent }
     ]
   },
+  {
+    path: '', component: BlankComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: '404', component: NotFoundComponent }
+    ]
+  }
 
 ];
 
