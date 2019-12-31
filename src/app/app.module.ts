13c46977ad0d253711df4d87c0d9dd3dd855991d
thanhtai9606 <thanhtai42@gmail.com>
2019-12-31 19:54:13 +0700

    Create Model + Angular8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { LayoutsModule } from './components/common/layouts/layouts.module';
 import { DashboardComponent } from './components/views/dashboard/dashboard.component';
 import { NotFoundComponent } from './components/common/not-found/not-found.component';
 import { LoginComponent } from './views/login/login.component';
+import { ProductComponent } from './views/product/product.component';
+import { CustomerComponent } from './views/customer/customer.component';
+import { SaleComponent } from './views/sale/sale.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    ProductComponent,
+    CustomerComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/acb-model.ts
@@ -0,0 +1,30 @@
+export class Customer {
+    CustomerID: number
+    CustomerName: string
+    Phone: string
+    Address: string
+    ModifiedDate: Date
+}
+
+export class Product {
+    ProductId: number
+    ProductName: string
+    Model: string
+    Inventory: number
+    Warranty: number
+    ModifiedDate: Date
+}
+
+export class Sale{
+    SoId: number
+    CustomerId: number
+    ProductId: number
+    WarrantyStart: Date
+    WarrantyEnd: Date
+    Quantity: number
+    ModifiedDate: Date
+    CreateBy: string
+
+    Products?: Product[]
+    Customer: Customer
+}
